329b4bde4621fca5f4212f7a899e8c3d9b7e85f8
MIQUEL-LOPEZ-ESCORIZA <ml9715@nyu.edu>
2024-11-14 03:50:27 -0500

    validation notebook

--- submit_job_dino_multigpu.sh
+++ submit_job_dino_multigpu.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=dino-pretrain
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
-#SBATCH --gres=gpu:v100:4             # Request 4 V100 GPUs
+#SBATCH --gres=gpu:rtx8000:4             # Request 4 V100 GPUs
 #SBATCH --time=48:00:00
-#SBATCH --mem-per-cpu=100G
+#SBATCH --mem-per-cpu=70G
 #SBATCH --output=slurm/%x-%A-%a-%j.out
 #SBATCH --error=slurm/%x-%A-%a-%j.err
 
@@ -17,4 +17,4 @@ export NCCL_DEBUG=INFO
 export PYTHONFAULTHANDLER=1
 
 # Run with 4 GPUs
-torchrun --nproc_per_node=4 --master_port 12346 train_multigpu.py --batch_size_per_gpu 128 --local_crops_number 0 --lr 0.00003 --output_dir "./checkpoints/RUN_2/"
+torchrun --nproc_per_node=4 --master_port 12346 train_multigpu.py --batch_size_per_gpu 128 --local_crops_number 0 --lr 0.00003 --output_dir "./checkpoints/RUN_3/"
